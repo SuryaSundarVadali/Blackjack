38e6da4d8fab41fcfa2bf38790ef3fbdf582d8ee
niketpatel-0208 <niket22100@iiitnr.edu.in>
2024-01-20 18:51:00 +0530

    routes updated

--- Blackjack.js
+++ Blackjack.js
@@ -35,75 +35,9 @@ function placeBet() {
         });
 }
 
-function bet50() {
-    fetch('/bet', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ amount: 50 }),
-    })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data.message);
-            updatePlayerChips();
-            resetUI();
-        });
-}
-
-function bet100() {
-    fetch('/bet', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ amount: 100 }),
-    })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data.message);
-            updatePlayerChips();
-            resetUI();
-        });
-}
-
-function bet150() {
-    fetch('/bet', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ amount: 150 }),
-    })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data.message);
-            updatePlayerChips();
-            resetUI();
-        });
-}
-
-function bet200() {
-    fetch('/bet', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ amount: 200 }),
-    })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data.message);
-            updatePlayerChips();
-            resetUI();
-        });
-}
-
 function resetUI() {
     betAmountInput.value = '';
     messageEl.textContent = '';
     sumEl.textContent = '';
     cardsEl.textContent = '';
 }
-
-// Add other functions...
